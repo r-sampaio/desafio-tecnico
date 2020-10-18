94fc7bbe408c52472d1ef00c4396635ac98f9c10
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-10-18 03:08:15 -0300

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -17,7 +18,7 @@ browser.get("https://www.acordacidade.com.br")
 
 # html_souce = browser.page_source
 # print(html_souce)
-
+procura = input(str('Digite o que produra: '))
 time.sleep(5)
 
 lupa = browser.find_element_by_xpath("//div[@class='busca']").click()
@@ -26,7 +27,9 @@ lupa = browser.find_element_by_xpath("//div[@class='busca']").click()
 busca = browser.find_element_by_xpath("//*[@placeholder='Pesquisar']")
 # busca = browser.findElement(By.id("inputbusca")).sendKeys("falta de agua")
 # busca.clear()
-busca.send_keys("falta de agua")
+# busca.send_keys("falta de agua")
+# busca.send_keys(sys.argv[0])
+busca.send_keys(procura)
 busca.send_keys(Keys.RETURN)
 
 time.sleep(5)
@@ -48,7 +51,15 @@ print(titulo.text)
 print()
 
 materia = browser.find_elements_by_xpath("//div[@id='tamanhotexto']//p")
+
+suprimir = browser.find_element_by_tag_name('sup')
+oculto = suprimir.text
+
 for c in materia:
-    print(c.text)
+        if c.text != oculto:
+            print(c.text)
+
+# for c in materia:
+#     print(c.text)
 
 browser.quit()
