9b82e268d226ec3d76079017dfab8e3853fc5666
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-10-18 14:10:45 -0300

    v2

--- README.md
+++ README.md
@@ -10,6 +10,46 @@
 
 ## Drivers
 
+Faça o download do driver para a versão do Google Chrome instalado na maquina.
+
 Chrome: [https://sites.google.com/a/chromium.org/chromedriver/downloads](https://sites.google.com/a/chromium.org/chromedriver/downloads)
 
-Firefox: [https://github.com/mozilla/geckodriver/releases](https://github.com/mozilla/geckodriver/releases)
+Extraia o arquivo para a pasta raiz do script: ```%PATH%/desafio-tecnico```
+
+## Modos de execução do script:
+
+##### Para alternar entre os modos de execução basta comentar ou e descomentar as opções desejadas 
+
+
+#### 1º : O script executa com um parametro de busca pré definido sendo ```falta de agua```.
+```python
+$ python parsing.py
+```
+
+#### 2º : O script executa a busca usando o parametro que foi passado via argumento.
+```python
+$ python parsing.py "falta de agua"
+```
+
+#### 3º : O script pede o parametro de busca via impute do usuario depois de executado.
+```python
+$ python parsing.py
+$ Digite o que produra: falta de agua
+```
+
+#### O resultado dos três exemplos acima seriam esse:
+
+```
+Embasa explica falta de água em diversos locais de Feira de Santana
+
+Acorda Cidade
+Em reposta às reclamações divulgadas no programa Acorda Cidade, enviadas via WhatsApp, sobre falta de água em alguns locais de Feira de Santana, a Empresa Baiana de Saneamentos e Águas (Embasa) enviou uma nota com explicações sobre a interrupção do fornecimento.
+
+Segundo a empresa, em função de uma falha elétrica no equipamento que bombeia água, houve uma interrupção no fornecimento de água em algumas localidades do distrito de Maria Quitéria, como Candeia Grossa. O equipamento já foi substituído e o abastecimento está sendo regularizado gradativamente.
+
+Em relação ao distrito da Matinha, estão sendo registradas baixas pressões na rede de distribuição de algumas localidades, em decorrência da elevação da temperaturas e o consequente aumento do consumo de água. Mas a Embasa já está fazendo ajustes operacionais para normalizar o abastecimento.
+
+Nos distritos de Jaguara e Bonfim de Feira, que são abastecidos pelo Sistema Integrado de Santo Estevão, ocorreram quedas de energia e um vazamento na adutora, que afetaram o fornecimento de água para a população local. O vazamento já foi corrigido, as tensões da rede elétrica de Estação de Tratamento de Água foi regularizada pela Coelba, e os técnicos estão trabalhando para normalizar o abastecimento nas próximas 48 horas.
+
+Em Ipuaçu, um equipamento de bombeamento quebrou, mas a Embasa já fez os reparos necessários e o abastecimento está sendo restabelecido.
+```
--- parsing.py
+++ parsing.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -5,61 +7,87 @@ from selenium.webdriver.common.keys import Keys
 import time
 import sys
 
+
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+
+# Executa o browser em background mostrando as informações no terminal
+chrome_options.add_argument("--headless") # Para desativar, comente essa linha
+
+# Desativa as mensagem de erro e infomações no terminal
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-browser = webdriver.Chrome(chrome_options=chrome_options, executable_path="chromedriver")
-# browser = webdriver.Chrome()
-# browser = webdriver.Firefox()
+# Cria a instancia com os paramentos passados para o browser e o caminho do driver Google
+browser = webdriver.Chrome(chrome_options=chrome_options, executable_path="./chromedriver")
+
+# Abre o browser no site passado por paramentro
 browser.get("https://www.acordacidade.com.br")
 
-# html_souce = browser.page_source
-# print(html_souce)
-procura = input(str('Digite o que produra: '))
+# Aguarda 5 segundos para que site carregue
 time.sleep(5)
 
+# Clica na lupa para que o campo de busca apareça
 lupa = browser.find_element_by_xpath("//div[@class='busca']").click()
-# busca = browser.find_element_by_name("busca")
-# busca = browser.find_element_by_xpath("//div[@class='busca']//input[@type='text']")
+
+# Seleciona o campo para que possa ser passado o termo para busca
 busca = browser.find_element_by_xpath("//*[@placeholder='Pesquisar']")
-# busca = browser.findElement(By.id("inputbusca")).sendKeys("falta de agua")
-# busca.clear()
-# busca.send_keys("falta de agua")
-# busca.send_keys(sys.argv[0])
-busca.send_keys(procura)
+
+# Para mudar o modo de busca basta comentar e descomentar os modos desejados
+
+busca.send_keys("falta de agua") # Paramentro pré definida
+
+# busca.send_keys(sys.argv[1].strip()) # Paramentro passado por argumento
+
+# procura = input(str('Digite o que produra: ')).strip() # Paramentro por input
+# busca.send_keys(procura) # Busca o paramentro que foi passado
+
+# Envia o termo de busca para pesquisa no site
 busca.send_keys(Keys.RETURN)
 
+# Aguarda 5 segundos para que site carregue
 time.sleep(5)
 
+# Seleciona a noticia em destaque
 noticia = browser.find_element_by_xpath("//div[@class='box-noticia2 box-noticia4 first']//h2[@class='tt_capa']")
+
+# Coleta a URL da noticia em destaque
 url = noticia.find_element_by_tag_name('a').get_attribute('href')
 
-# href = browser.find_element_by_id("noticias")
-# print(noticias.text)
-
+# Abre a URL que foi coletada da noticia em destaque
 browser.get(url)
 
-os.system('cls')
+# Limpa a tela do terminal
+if os.name == 'nt':
+    os.system('cls') 
+else:
+    os.system('clear')
 
-# print(url)
-
+# Coleta e mostra no terminal o titulo da materia
 titulo = browser.find_element_by_class_name("titulo")
 print(titulo.text)
 print()
 
+# Coleta e mostra no terminal o body da materia
 materia = browser.find_elements_by_xpath("//div[@id='tamanhotexto']//p")
 
-suprimir = browser.find_element_by_tag_name('sup')
-oculto = suprimir.text
-
+# Passa por todas as TAGs <p> dendo do body de conteudo 
 for c in materia:
-        if c.text != oculto:
-            print(c.text)
+    print(c.text)
+
+
+# O siste mudou a Forma de apresentação de materias mais recentes onde usava a 
+# primeita TAG <p> para mostrar a localidade da materia ou nome do autor agora 
+# esta sendo usada em algumas como parte da materia e outras nao aparece.
+
+# Para visualizar materias antigas comente da linha 73 ~ 74 descomente 83 ~ 88
+
+# suprimir = browser.find_element_by_tag_name('sup')
+# oculto = suprimir.text
 
 # for c in materia:
-#     print(c.text)
+#         if c.text != oculto:
+#             print(c.text)
 
+print()
+
+# Encerra o processo do browser
 browser.quit()
